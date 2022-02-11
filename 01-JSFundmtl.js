01a99799adb9bf9053a48feca3567db77ac0420a
Virajj28 <viraj28j@gmail.com>
2022-02-11 15:36:29 +0530

    selected /rtc code

--- 01-JSFundmtl.js
+++ 01-JSFundmtl.js
@@ -1,2 +1,22 @@
-console.log("Practice JS")
+// console.log("Practice JS")
 
+// Explaining useEffect Hook
+ueEffect ( ()=> {
+    fetch("https://..")
+    .then((res)=> res.json)
+    .then((data)=>{
+        console.log(data);
+    })
+},[])
+
+
+// Explaining Function Callback
+function calculate(num1,num2, callbackFunc){
+    return callbackFunc(num1,num2);
+}
+
+function calcSum (num1,num2){
+    return num1 + num2;
+}
+
+console.log(calculate(2,3, calcSum));
