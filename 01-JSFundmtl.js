4f034ada11c97383f61bcc832a15fa19302b12c8
Virajj28 <viraj28j@gmail.com>
2022-02-11 14:48:36 +0530

    undo

--- 01-JSFundmtl.js
+++ 01-JSFundmtl.js
@@ -1,11 +1,2 @@
-// console.log("Practice JS")
+console.log("Practice JS")
 
-function calc (num1,num2,callbackFunction){
-    return callbackFunction(num1, num2);
-}
-
-function calcSum(num1,num2){
-    return num1+num2;
-}
-
-console.log(calc (5,5,calcSum));
