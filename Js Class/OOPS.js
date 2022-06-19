e012c00bd80611a910fe300d9af1d2c0de56a0de
Virajj28 <viraj28j@gmail.com>
2022-06-19 19:53:01 +0530

    objs literl for classes

--- Js Class/OOPS.js
+++ Js Class/OOPS.js
@@ -1,1 +1,14 @@
 // Functions associated in objects are called methods in classes.
+var userOne = {
+    email: 'geek@Viraj.com',
+    name: 'Viraj',
+    login(){
+        console.log(this.email, 'has logged in');
+    },
+    logout(){
+        console.log(this.email, 'has logged out');
+    }
+};
+
+userOne.login();
+userOne.logout();
