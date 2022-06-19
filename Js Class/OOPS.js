9a28893e9a6492b4f4e18854361ef0be565146a3
Virajj28 <viraj28j@gmail.com>
2022-06-19 20:11:44 +0530

    class constrctrs

--- Js Class/OOPS.js
+++ Js Class/OOPS.js
@@ -1,18 +1,38 @@
 // Functions associated in objects are called methods in classes.
-var userOne = {
-    email: 'geek@Viraj.com',
-    name: 'Viraj',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
-    }
-};
+// var userOne = {
+//     email: 'geek@Viraj.com',
+//     name: 'Viraj',
+//     login(){
+//         console.log(this.email, 'has logged in');
+//     },
+//     logout(){
+//         console.log(this.email, 'has logged out');
+//     }
+// };
 
-userOne.login();
-userOne.logout();
+// userOne.login();
+// userOne.logout();
 
-// Updatiing Properties
+// Updatiing Properties in Objects
 // userOne.name = 'Yo';
 // console.log(userOne.name);
+
+
+//// Class constructors
+// the 'new' keyword is used to create an object from a class.
+// - creates a new empty object
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
+
+class User {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+    }
+}
+
+var userOne = new User('geek@gmail.com', 'Geek');
+var userTwo = new User('nervebody@yahoo.com','Nervebody');
+
+console.log(userOne);
+console.log(userTwo);
