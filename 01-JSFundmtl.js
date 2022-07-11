4c1368d61173345002cb27c2deb36a2e889c1fdd
Virajj28 <viraj28j@gmail.com>
2022-07-11 19:45:17 +0530

    throwing new error revise

--- 01-JSFundmtl.js
+++ 01-JSFundmtl.js
@@ -31,3 +31,21 @@ setTimeout(function() {
       alert( "error is caught here!" );
     }
   }, 1000)
+
+
+  // throwing our new Error
+  let json = '{ "age": 30 }'; // incomplete data
+
+try {
+
+  let user = JSON.parse(json); // <-- no errors
+
+  if (!user.name) {
+    throw new SyntaxError("Incomplete data: no name"); // (*)
+  }
+
+  alert( user.name );
+
+} catch (err) {
+  alert( "JSON Error: " + err.message ); // JSON Error: Incomplete data: no name
+}
