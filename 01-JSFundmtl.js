37a4ff62ba4bd5e8af536ccd26a12373e710a134
Virajj28 <viraj28j@gmail.com>
2022-07-11 19:49:30 +0530

    try,c,fnlly

--- 01-JSFundmtl.js
+++ 01-JSFundmtl.js
@@ -73,3 +73,13 @@ try {
   }
 
 }
+
+// try,catch,finally
+try {
+  alert( 'try' );
+  if (confirm('Make an error?')) BAD_CODE();
+} catch (err) {
+  alert( 'catch' );
+} finally {
+  alert( 'finally' );
+}
