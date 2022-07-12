45c9e9e88aaac2d7dce4b61630eb63e879d8e746
Virajj28 <viraj28j@gmail.com>
2022-07-12 23:01:00 +0530

    types of lints

--- Code Quality/linters.js
+++ Code Quality/linters.js
@@ -1,3 +1,7 @@
 // Automated Linters
 // Linters are tools that can automatically check the style of your code and make improving suggestions.
 // The great thing about them is that style-checking can also find some bugs, like typos in variable or function names. Because of this feature, using a linter is recommended even if you don’t want to stick to one particular “code style”.
+
+// JSLint – one of the first linters.
+// JSHint – more settings than JSLint.
+// ESLint – probably the newest one.
